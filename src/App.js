eca52504b9e2405d5fb7058d67e92e12298356ae
krishna saparia <krishnasaparia43045@gmail.com>
2021-02-18 19:15:49 -0500

    Add Page Loading Animation.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 // import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react'
 import {BrowserRouter as Router } from 'react-router-dom'
 import Home from './pages';
-function App() {
+import { css } from "@emotion/core";
+import ClimbingBoxLoader from 'react-spinners/ClimbingBoxLoader'
 
+const override = css`
+  background-color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+`;
+
+function App() {
+  const[loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000)
+  },[])
   return (
+    
     <Router>
+    {loading ? (
+      <ClimbingBoxLoader size={30} color={'#773db4'} loading={loading} css={override}/>
+    ) : (
       <Home />
+    )}
     </Router>
   );
 }
