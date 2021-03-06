5747a5169aa10689d263b3424770e4db8f7de8b9
krishna saparia <krishnasaparia43045@gmail.com>
2021-03-06 18:09:53 -0500

    Made minor changes

--- src/Components/Sidebar/SidebarElements.js
+++ src/Components/Sidebar/SidebarElements.js
@@ -86,3 +86,20 @@ export const SidebarRoute = styled(LinkR)`
         background: #fff;
         color: #010606;
     }`
+
+    export const SidebarItem = styled.a`
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 1.5rem;
+        text-decoration: none;
+        list-style: none;
+        transition: 0.2s ease-in-out;
+        text-decoration: none;
+        color: #fff;
+        cursor: pointer;
+        
+        &:hover {
+            color: #01bf71;
+            transition: 0.2s ease-in-out;
+        }`
--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {SidebarContainer, CloseIcon, Icon, SidebtnWrap, SidebarRoute, SidebarLink, SidebarWrapper, SidebarMenu} from './SidebarElements';
+import {SidebarContainer, CloseIcon, Icon, SidebtnWrap, SidebarRoute, SidebarLink, SidebarWrapper, SidebarMenu, SidebarItem} from './SidebarElements';
 
 const Sidebar = ({isOpen, toggle}) => {
     return (
@@ -9,11 +9,12 @@ const Sidebar = ({isOpen, toggle}) => {
             </Icon>
             <SidebarWrapper>
                 <SidebarMenu>
+                <SidebarItem href='https://drive.google.com/file/d/1vL1N_WHi3qS8vAv7ynacovkLcYrK4ppL/view?usp=sharing/' target="_blank" aria-label="Resume">.toResume()</SidebarItem>
                     <SidebarLink to='about' onClick={toggle}>
                         .about()
                     </SidebarLink>
-                    <SidebarLink to='resume' onClick={toggle}>
-                        .toResume()
+                    <SidebarLink to='education' onClick={toggle}>
+                        .toEducation()
                     </SidebarLink>
                     <SidebarLink to='projects' onClick={toggle}>
                         .toProjects()
